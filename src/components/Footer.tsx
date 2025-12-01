ef084e5a57d5ec609501956f93baf64bbdef0edc
littl3fo0t <thomas.brun.1703@hotmail.com>
2025-12-01 10:26:00 -0400

    Fixed spacing issue with icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,29 +2,23 @@
 
 const Footer: React.FC = () => {
     return (
-        <footer className="footer has-text-centered">
-            <div className="container is-narrow">
-                <div className="content">
-                    <div className="columns">
-                        <div className="column">
-                            <a href="https://www.linkedin.com/in/thomasbrun1/" target="_blank">
-                                <span className="icon is-large">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </span>
-                            </a>
-                        </div>
-                        <div className="column">
-                            <span className="icon is-large">
-                                <i className="fab fa-github"></i>
-                            </span>
-                        </div>
-                        <div className="column">
-                            <span className="icon is-large">
-                                <i className="far fa-envelope"></i>
-                            </span>
-                        </div>
-                    </div>
-                </div>
+        <footer className="footer mt-4" data-theme="dark">
+            <div className="container is-flex is-justify-content-center">
+                <a href="https://www.linkedin.com/in/thomasbrun1/" target="_blank">
+                    <span className="icon is-large has-text-white">
+                        <i className="fab fa-linkedin-in fa-2x"></i>
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/in/thomasbrun1/" target="_blank">
+                    <span className="icon is-large has-text-white">
+                        <i className="fab fa-github fa-2x"></i>
+                    </span>
+                </a>
+                <a href="https://www.linkedin.com/in/thomasbrun1/" target="_blank">
+                    <span className="icon is-large has-text-white">
+                        <i className="far fa-envelope fa-2x"></i>
+                    </span>
+                </a>
             </div>
         </footer>
     );
